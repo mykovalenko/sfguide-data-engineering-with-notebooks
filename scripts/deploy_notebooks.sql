7133961ed2239c19ac2189296bd9f57d9fd7f212
mykovalenko <mykovalenko@futurica.es>
2024-10-12 18:07:43 +0200

    MK: Naming mapping

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -5,6 +5,8 @@ Hands-On Lab: Intro to Data Engineering with Notebooks
 Script:       deploy_notebooks.sql
 Author:       Jeremiah Hansen
 Last Updated: 6/11/2024
+Editor:       Mykola Kovalenko
+Last Updated: 10/12/2024
 -----------------------------------------------------------------------------*/
 
 -- See https://docs.snowflake.com/en/LIMITEDACCESS/execute-immediate-from-template
@@ -12,16 +14,16 @@ Last Updated: 6/11/2024
 -- Create the Notebooks
 --USE SCHEMA {{env}}_SCHEMA;
 
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}_06_load_excel_files"')
-    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/06_load_excel_files/'
-    QUERY_WAREHOUSE = 'DEMO_WH'
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO"."PYPLINES_{{env}}"."NB_Demo_Pyplines_{{env}}_06_load_excel_files"')
+    FROM '@"DEMO"."PYPLINES"."REPO_GIT_DEMO_PYPLINES"/branches/"{{branch}}"/notebooks/06_load_excel_files/'
+    QUERY_WAREHOUSE = 'WH_DEMO_PYPLINES'
     MAIN_FILE = '06_load_excel_files.ipynb';
 
-ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}_06_load_excel_files" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "DEMO"."PYPLINES_{{env}}"."NB_Demo_Pyplines_{{env}}_06_load_excel_files" ADD LIVE VERSION FROM LAST;
 
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics"')
-    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
-    QUERY_WAREHOUSE = 'DEMO_WH'
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO"."PYPLINES_{{env}}"."NB_Demo_Pyplines_{{env}}_07_load_daily_city_metrics"')
+    FROM '@"DEMO"."PYPLINES"."REPO_GIT_DEMO_PYPLINES"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
+    QUERY_WAREHOUSE = 'WH_DEMO_PYPLINES'
     MAIN_FILE = '07_load_daily_city_metrics.ipynb';
 
-ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "DEMO"."PYPLINES_{{env}}"."NB_Demo_Pyplines_{{env}}_07_load_daily_city_metrics" ADD LIVE VERSION FROM LAST;
